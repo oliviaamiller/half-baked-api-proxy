0e895ba0fec09f98afa6de016393f71d4b4e34b5
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-14 14:18:05 -0800

    tracking state in pokemon search

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -2,6 +2,10 @@ import { useState } from 'react';
 
 export default function PokemonSearch() {
       // you'll need to track your pokemon search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
+  const [search, setSearch] = useState('');
+  const [formName, setFormName] = useState('Pikachu');
+  const [pokemon, setPokemon] = useState([]);
+
   
   async function handlePokemonSubmit(e) {
     e.preventDefault();
