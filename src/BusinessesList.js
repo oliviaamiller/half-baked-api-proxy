a69076ca2d57e5ce978432e9a9491d42168038f6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-15 16:39:04 -0800

    mapping through businesses

--- src/BusinessesList.js
+++ src/BusinessesList.js
@@ -1,6 +1,12 @@
 
-export default function BusinessesList() {
+export default function BusinessesList({ businesses }) {
   return (
-    <div>BusinessesList</div>
+    <div>
+      {businesses.map((business, i) =>
+        <div key={`${business}-${i}`}>
+          <img src={business.image_url} />
+          <p>{business.name}</p>
+        </div>)}
+    </div>
   );
 }
