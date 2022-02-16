a7da9c7825ba3d860748c25da80a707f6f343471
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-16 11:18:25 -0800

    added text to test

--- src/ForecastList.js
+++ src/ForecastList.js
@@ -3,7 +3,8 @@ export default function ForecastList({ forecasts, parseTime }) {
     <div>
       {forecasts.map((forecast, i) =>
         <div key={`${forecast}-${i}`}>
-          <p>date: {parseTime(forecast.dt)}</p>
+          <img />
+          <p className='search-name'>{parseTime(forecast.dt)}</p>
           <p>{forecast.weather[0].description}</p>
           <p>low: {forecast.temp.min}°F</p>
           <p>high: {forecast.temp.max}°F</p>
