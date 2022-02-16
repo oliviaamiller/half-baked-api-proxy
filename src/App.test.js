ed8edbcb17e929d51fd696a58a7b77af4a3e41e0
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-16 11:23:11 -0800

    updated test text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders search for weather by city link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Search for weather by city/i);
+  const linkElement = screen.getByText(/Search weather by city/i);
   expect(linkElement).toBeInTheDocument();
 });
